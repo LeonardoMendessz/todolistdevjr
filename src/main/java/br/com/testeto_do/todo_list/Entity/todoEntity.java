3aaea90931c5506e2e5b24888654c3dc2b31cba3
showmisco23@icloud.com <showmisco23@icloud.com>
2025-07-21 18:39:35 -0300

    nomeei a tabela da entidade

--- src/main/java/br/com/testeto_do/todo_list/Entity/todoEntity.java
+++ src/main/java/br/com/testeto_do/todo_list/Entity/todoEntity.java
@@ -1,11 +1,9 @@
 package br.com.testeto_do.todo_list.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "todo")
 public class todoEntity {
     private String nome;
     private String descricao;
