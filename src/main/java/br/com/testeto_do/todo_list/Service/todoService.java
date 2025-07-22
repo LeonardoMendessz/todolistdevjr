ef293639ada9781288c72a09c8307957d44ffcd6
showmisco23@icloud.com <showmisco23@icloud.com>
2025-07-21 22:06:23 -0300

    criei o service completo com as regras de negocio

--- src/main/java/br/com/testeto_do/todo_list/Service/todoService.java
+++ src/main/java/br/com/testeto_do/todo_list/Service/todoService.java
@@ -1,8 +1,12 @@
 package br.com.testeto_do.todo_list.Service;
 
+import br.com.testeto_do.todo_list.Entity.todoEntity;
 import br.com.testeto_do.todo_list.Repository.todoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class todoService {
@@ -14,6 +18,26 @@ public class todoService {
         this.todoRepository = todoRepository;
     }
 
+    public List<todoEntity> criarTarefa(todoEntity todo){
+        todoRepository.save(todo); // vamos retornar a lista posteriormente
+        return listarTarefas();
+    }
+
+
+    public List<todoEntity> listarTarefas(){
+        Sort sort = Sort.by("prioridade").descending();
+        return todoRepository.findAll(sort);
+    }
+
+    public List<todoEntity> atualizarTarefa(todoEntity todo){
+        todoRepository.save(todo);
+        return listarTarefas();
+    }
+
+    public List<todoEntity> deletarTarefa(Long id){
+        todoRepository.deleteById(id);
+        return listarTarefas();
+    }
 
 
 }
