42b03d600147c2847777e369d368c25c2dd54d5f
showmisco23@icloud.com <showmisco23@icloud.com>
2025-07-21 22:39:09 -0300

    criei o controller da api

---
+++ src/main/java/br/com/testeto_do/todo_list/Controller/todoController.java
@@ -0,0 +1,38 @@
+package br.com.testeto_do.todo_list.Controller;
+
+import br.com.testeto_do.todo_list.Entity.todoEntity;
+import br.com.testeto_do.todo_list.Service.todoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todo")
+public class todoController {
+
+    private todoService todoService;
+
+    public todoController(todoService todoService){
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    public List<todoEntity> criarTarefa(@RequestBody  todoEntity todo){
+        return todoService.criarTarefa(todo);
+    }
+
+    @GetMapping
+    public List<todoEntity> listarTarefa(){
+        return todoService.listarTarefas();
+    }
+
+    @PutMapping("/{id}")
+    public List<todoEntity> atualizarTarefa(@RequestBody todoEntity todo){
+        return todoService.atualizarTarefa(todo);
+    }
+
+    @DeleteMapping("/{id}")
+    public List<todoEntity> deletarTarefa(@PathVariable Long id){
+        return todoService.deletarTarefa(id);
+    }
+}
